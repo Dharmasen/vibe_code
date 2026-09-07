0b8bcf49e2c437ddf93d16d6d9baad8d43c4ec44
Dharmasen Yadav <mdsyadav.5i6@gmail.com>
2026-09-07 14:29:39 +0530

    updated port number for app

--- node-app/app.js
+++ node-app/app.js
@@ -9,7 +9,7 @@ const server = http.createServer((req, res) => {
     res.end(routes.getMessage());
 });
 
-const PORT = 5000;
+const PORT = 6000;
 
 server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
