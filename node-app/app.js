c78d99122e25786958f3f1ff2346b4e05163f315
Dharmasen Yadav <mdsyadav.5i6@gmail.com>
2026-09-17 21:30:39 +0530

    Save to session branch - 2026-09-17T16:00:31.453Z

--- node-app/app.js
+++ node-app/app.js
@@ -1,40 +1,16 @@
 const http = require('http');
-const routes = require('./routes');;;
+const routes = require('./routes');
 
-const PORT = 4000;..
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
 
-/**
- * Process incoming requests
- * @param {Object} req
- * @param {Object} res
- */
-const requestHandler = (req, res) => {
-    try {
-        const message = routes.getMessage();
+    res.end(routes.getMessage());
+});
 
-        res.writeHead(200, {
-            'Content-Type': 'text/plain'
-        });
-
-        res.end(message);
-    } catch (error) {
-        res.writeHead(500, {
-            'Content-Type': 'text/plain'
-        });
-
-        res.end('Internal Server Error');
-
-        console.error('Error:', error.message);
-    }
-};
-
-const server = http.createServer(requestHandler);
+const PORT = 6000;
 
 server.listen(PORT, () => {
-    console.log('--------------------------------');
-    console.log('Server Status : Running');
-    console.log(`Port          : ${PORT}`);
-    console.log(`Address       : http://localhost:${PORT}`);
-    console.log('--------------------------------');
+    console.log(`Server running at http://localhost:${PORT}`);
 });
-`
